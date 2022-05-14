e1d95bf6a1f4dabaaf873dfbe64c160a6c17df6e
STROAD <koseok0611@naver.com>
2022-05-14 12:08:43 +0900

    스크롤이 끝까지 안내려가는 문제 수정

--- crawling.py
+++ crawling.py
@@ -23,28 +23,21 @@ search.send_keys(Keys.ENTER)
 # 스크롤 전 높이
 before_h = browser.execute_script("return window.scrolly")
 
-# 무한 스크롤
-while True:
+# PAGE_DOWN버튼을 7번 눌러 아래로 스크롤
+for i in range(0, 7):
     # 맨 아래로 스크롤을 내린다
-    browser.find_element_by_css_selector("body").send_keys(Keys.END)
+    browser.find_element_by_css_selector("body").send_keys(Keys.PAGE_DOWN)
 
     # 스크롤 사이 페이지 로딩 시간
     time.sleep(1)
 
-    # 스크롤 후 높이
-    after_h = browser.execute_script("return window.scrolly")
+# 상품 정보 div
+items = browser.find_elements_by_css_selector(".basicList_info_area__17Xyo")
 
-    if after_h == before_h:
-        break
-    before_h = after_h
-
-    # 상품 정보 div
-    items = browser.find_elements_by_css_selector(".basicList_info_area__17Xyo")
-
-    for item in items:
-        name = item.find_element_by_css_selector(".basicList_link__1MaTN").text
-        price = item.find_element_by_css_selector(".price_num__2WUXn").text
-        link = item.find_element_by_css_selector(
-            ".basicList_link__1MaTN > a"
-        ).get_attribute("href")
-        print(name, price, link)
+for item in items:
+    name = item.find_element_by_css_selector(".basicList_link__1MaTN").text
+    price = item.find_element_by_css_selector(".price_num__2WUXn").text
+    link = item.find_element_by_css_selector(
+        ".basicList_link__1MaTN > a"
+    ).get_attribute("href")
+    print(name, price, link)
