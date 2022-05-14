aae815c654dd6c99a31776e46c9a6592676ee94a
STROAD <koseok0611@naver.com>
2022-05-14 12:07:44 +0900

    코드정리

--- crawling.py
+++ crawling.py
@@ -3,21 +3,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # 브라우저 생성
-browser = webdriver.Chrome('C:/chromedriver.exe')
+browser = webdriver.Chrome("C:/chromedriver.exe")
 
 # 웹 사이트 열기
-browser.get('https://www.naver.com')
-browser.implicitly_wait(10) # 로딩 끝날 때 까지 10초 기다려줌
+browser.get("https://www.naver.com")
+browser.implicitly_wait(10)  # 로딩 끝날 때 까지 10초 기다려줌
 
 # 쇼핑 메뉴 클릭하기
-browser.find_element_by_css_selector('a.nav.shop').click()
+browser.find_element_by_css_selector("a.nav.shop").click()
 
 # 검색창 클릭
-search = browser.find_element_by_css_selector('input.co_srh_input._input')
+search = browser.find_element_by_css_selector("input.co_srh_input._input")
 search.click()
 
 # 검색어 입력
-search.send_keys('옷')
+search.send_keys("옷")
 search.send_keys(Keys.ENTER)
 
 # 스크롤 전 높이
@@ -41,8 +41,10 @@ while True:
     # 상품 정보 div
     items = browser.find_elements_by_css_selector(".basicList_info_area__17Xyo")
 
-    for item in items :
+    for item in items:
         name = item.find_element_by_css_selector(".basicList_link__1MaTN").text
         price = item.find_element_by_css_selector(".price_num__2WUXn").text
-        link = item.find_element_by_css_selector(".basicList_link__1MaTN > a").get_attribute('href')
+        link = item.find_element_by_css_selector(
+            ".basicList_link__1MaTN > a"
+        ).get_attribute("href")
         print(name, price, link)
